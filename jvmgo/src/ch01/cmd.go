e565d5d79ceba484bef3e687ded9b5907f589013
csucoderlee <csucoderlee@foxmail.com>
2021-09-21 23:25:25 +0800

    go run *.go

--- jvmgo/src/ch01/cmd.go
+++ jvmgo/src/ch01/cmd.go
@@ -14,18 +14,6 @@ type Cmd struct {
 	args        []string
 }
 
-func main() {
-	cmd := parseCmd()
-
-	if cmd.versionFlag {
-		fmt.Println("version 0.0.1")
-	} else if cmd.helpFlag || cmd.class == "" {
-		printUsage()
-	} else {
-		startJVM(cmd)
-	}
-}
-
 func parseCmd() *Cmd {
 	cmd := &Cmd{}
 
